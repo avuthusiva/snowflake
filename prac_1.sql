15d99b8eafd3c20e643b3c87918acee471fd7f6d
AVUTHU SIVA VARDHANA REDDY <53142715+avuthusiva@users.noreply.github.com>
2026-01-19 13:14:41 +0000

    Added new table

--- prac_1.sql
+++ prac_1.sql
@@ -29,3 +29,30 @@ select * from hotel_bookings;
 select case when "check_in_date" like '%/%' then to_char(to_date("check_out_date"),'YYYY-MM-DD')
             when "check_in_date" like '%-%' then to_char(to_date("check_out_date"),'YYYY-MM-DD') end check_in_date
  from hotel_bookings;    
+select 'Hi SIVA REDDY' txt,snowflake.cortex.translate(txt,'en','hi');
+select * from directory(@int_stage);
+select * from table(infer_schema(location=>'@int_stage/files/csv/swiggy_data.csv',file_format=>'csv_parse_header'));
+select $1,$2,$3,$4,$5,$6,$7,$8,$9,$10 from @int_stage/files/csv/swiggy_data.csv (file_format=>'csv_skip_header');
+create table swiggy_data
+(
+    state text,
+    city text,
+    order_date date,
+    restaurant_name text,
+    location text,
+    category text,
+    dish_name text,
+    price number(6,2),
+    rating number(2,1),
+    rating_count number,
+    filename text,
+    file_row_number number,
+    create_date timestamp default current_timestamp
+);
+copy into swiggy_data from (
+select $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,metadata$filename,metadata$file_row_number,current_timestamp() from 
+@int_stage/files/csv/swiggy_data.csv (file_format=>'csv_skip_header'));
+select * from swiggy_data;
+select state,city,restaurant_name,count(*) from swiggy_data
+group by 1,2,3
+order by 1;
