b01d16115a23f1b666647c30ec6f115efeda9cdb
AVUTHU SIVA VARDHANA REDDY <53142715+avuthusiva@users.noreply.github.com>
2026-01-19 11:26:27 +0000

    Added new changes

--- prac_1.sql
+++ prac_1.sql
@@ -18,3 +18,14 @@ copy into hotel_bookings from
 (select $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,metadata$filename,metadata$file_row_number 
 from @int_stage/files/csv/hotel_bookings_raw.csv (file_format =>csv_skip_header)); 
 select * from hotel_bookings;
+select * from table(information_schema.copy_history(table_name=>'hotel_bookings'
+,start_time=>dateadd(hour,-1,current_timestamp())));
+select * from hotel_bookings;
+select * from hotel_bookings where "booking_id" is null;
+delete from hotel_bookings where "booking_id" is null;
+select * from hotel_bookings where "hotel_id" is null;
+delete from hotel_bookings where "hotel_id" is null;
+select * from hotel_bookings;
+select case when "check_in_date" like '%/%' then to_char(to_date("check_out_date"),'YYYY-MM-DD')
+            when "check_in_date" like '%-%' then to_char(to_date("check_out_date"),'YYYY-MM-DD') end check_in_date
+ from hotel_bookings;    
